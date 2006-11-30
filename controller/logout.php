cc466034e6e67a4c3cd054b0855cdb2501144c18
jcollins1 <alphadevx@users.noreply.github.com>
2006-11-30 17:45:52 +0000

    Added a file remotely

---
+++ controller/logout.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once '../config/config.conf';
+require_once '../model/person_object.inc';
+require_once '../view/person.inc';
+require_once '../config/db_connect.inc';
+require_once '../controller/Controller.inc';
+
+/**
+ *
+ * Logout controller that removes the current user object to the session
+ * 
+ * @package Alpha Admin
+ * @author John Collins <john@design-ireland.net>
+ * @copyright 2006 John Collins
+ * @todo logging of user logout times
+ * 
+ */
+class logout extends Controller
+{
+	/**
+	 * constructor to set up the object
+	 */
+	function logout() {
+		global $sysURL;
+		
+		// ensure that the super class constructor is called
+		$this->Controller();
+		
+		$this->person_object = new person_object();
+		$this->person_view = new person($this->person_object);
+		$this->set_BO($this->person_object);
+		
+		// set up the title and meta details
+		$this->set_title("Logged out successfully.");
+		$this->set_description("logout page.");
+		$this->set_keywords("logout,logon");
+		
+		$_SESSION = array();
+		
+		session_destroy();
+		
+		$this->display_page_head();
+		
+		echo '<center><p class="success">You have successfully logged out of the system.</p><br>';
+		
+		echo '<a href="'.$sysURL.'">Home Page</a></center>';
+		
+		$this->display_page_foot();
+	}	
+}
+
+$controller = new logout();
+
+?>
