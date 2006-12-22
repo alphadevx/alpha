3712434181572510700321aa843d6bd8e4ff8748
john <john@1970b06b-b575-2e47-bfa4-2824af2bf091>
2006-12-22 16:16:48 +0000

    Updated URL for force-frame script.

--- trunk/controller/view_article_title.php
+++ trunk/controller/view_article_title.php
@@ -88,7 +88,7 @@ class view_article_title extends Controller
 		
 		echo '<link rel="StyleSheet" type="text/css" href="'.$sysURL.'/config/css/'.$sysTheme.'.css.php">';
 		if(!isset($_GET["no-forceframe"]) && $sysForceFrame)
-			echo '<script language="JavaScript" src="'.$sysURL.'/scripts/force-frame.js"></script>';
+			echo '<script language="JavaScript" src="'.$sysURL.'/alpha/scripts/force-frame.js"></script>';
 		
 		if ($sysUseWidgets) {
 			echo '<script language="JavaScript" src="'.$sysURL.'/alpha/scripts/addOnloadEvent.js"></script>';
