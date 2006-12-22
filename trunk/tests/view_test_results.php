90501691c44589577c2bfa8b95b892b0d509ddb0
john <john@1970b06b-b575-2e47-bfa4-2824af2bf091>
2006-12-22 23:54:47 +0000

    Controller for viewing all of the unit test results

---
+++ trunk/tests/view_test_results.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once '../../config/config.conf';
+require_once $sysRoot.'alpha/model/person_object.inc';
+require_once $sysRoot.'alpha/view/person.inc';
+require_once $sysRoot.'config/db_connect.inc';
+require_once $sysRoot.'alpha/controller/Controller.inc';
+require_once $sysRoot.'alpha/tests/Enum_Test.php';
+require_once 'PHPUnit.php';
+
+/**
+ *
+ * Controller which displays all of the unit test results
+ * 
+ * @package Alpha Core Unit Tests
+ * @author John Collins <john@design-ireland.net>
+ * @copyright 2006 John Collins 
+ * 
+ */
+class view_test_results extends Controller
+{	
+	/**
+	 * constructor to set up the object
+	 */
+	function view_test_results() {
+		// ensure that the super class constructor is called
+		$this->Controller();		
+		
+		// set up the title and meta details
+		$this->set_title("Alpha Core Unit Test Results");		
+		
+		$this->display_page_head();
+		
+		echo "<h2>Core Complex Data Types</h2>";
+		
+		//------------------------------------------------
+		echo "<h3>Enum:</h3>";
+		
+		$suite  = new PHPUnit_TestSuite("Enum_Test");
+		$result = PHPUnit::run($suite);
+
+		if($result->wasSuccessful())
+			echo '<span class="success">'.$result->toHTML().'</span>';
+		else
+			echo '<span class="warning">'.$result->toHTML().'</span>';
+		
+		
+		$this->display_page_foot();
+	}	
+}
+
+$controller = new view_test_results();
+
+?>
