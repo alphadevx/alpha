c913f18df93c3813d539bf97afa4118250248147
john <alphadevx@users.noreply.github.com>
2006-12-23 00:55:15 +0000

    Locked down page for admins only.

--- tests/view_test_results.php
+++ tests/view_test_results.php
@@ -29,6 +29,11 @@ class view_test_results extends Controller
 		// ensure that the super class constructor is called
 		$this->Controller();		
 		
+		$this->set_visibility("Administrator");
+		if(!$this->check_rights()){			
+			exit;
+		}
+		
 		// set up the title and meta details
 		$this->set_title("Alpha Core Unit Test Results");		
 		
@@ -59,7 +64,54 @@ class view_test_results extends Controller
 			echo '<span class="warning">'.$result->toHTML().'</span>';
 		
 		$this->display_page_foot();
-	}	
+	}
+	
+	/**
+	 * method to display the page head with pageination links
+	 */
+	function display_page_head() {
+		global $sysURL;
+		global $sysTheme;
+		global $sysUseWidgets;
+		global $sysRoot;		
+		
+		echo '<html>';
+		echo '<head>';
+		echo '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">';
+		echo '<title>'.$this->get_title().'</title>';
+		echo '<meta name="Keywords" content="'.$this->get_keywords().'">';
+		echo '<meta name="Description" content="'.$this->get_description().'">';
+		echo '<meta name="Author" content="john collins">';
+		echo '<meta name="copyright" content="copyright ">';
+		echo '<meta name="identifier" content="http://'.$sysURL.'/">';
+		echo '<meta name="revisit-after" content="7 days">';
+		echo '<meta name="expires" content="never">';
+		echo '<meta name="language" content="en">';
+		echo '<meta name="distribution" content="global">';
+		echo '<meta name="title" content="'.$this->get_title().'">';
+		echo '<meta name="robots" content="index,follow">';
+		echo '<meta http-equiv="imagetoolbar" content="no">';			
+		
+		echo '<link rel="StyleSheet" type="text/css" href="'.$sysURL.'/config/css/'.$sysTheme.'.css.php">';
+		
+		if ($sysUseWidgets) {
+			echo '<script language="JavaScript" src="'.$sysURL.'/alpha/scripts/addOnloadEvent.js"></script>';
+			require_once $sysRoot.'alpha/view/widgets/button.js.php';
+		}
+		
+		echo '</head>';
+		echo '<body>';
+			
+		echo '<h1>'.$this->get_title().'</h1>';
+		
+		if (isset($_SESSION["current_user"])) {	
+			echo '<p>You are logged in as '.$_SESSION["current_user"]->get_displayname().'.  <a href="'.$sysURL.'/alpha/controller/logout.php">Logout</a></p>';
+		}else{
+			echo '<p>You are not logged in</p>';
+		}
+		
+		echo '<p align="center"><a href="'.$sysURL.'/alpha/controller/ListBusinessObjects.php">Administration Home Page</a></p><br>';
+	}
 }
 
 $controller = new view_test_results();
