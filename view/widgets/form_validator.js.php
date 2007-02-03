153a52354647b5f3a9ddf9f3114154ea4fd81ca7
john <alphadevx@users.noreply.github.com>
2007-02-03 22:18:05 +0000

    Added check to BO->transient_attributes before carry out an action on these ignored attributes.

--- view/widgets/form_validator.js.php
+++ view/widgets/form_validator.js.php
@@ -44,7 +44,7 @@ class form_validator
 		$properties = get_object_vars($this->BO);		
 		
 		foreach(array_keys($properties) as $prop) {
-			if(!in_array($prop, $this->BO->default_attributes)) {
+			if(!in_array($prop, $this->BO->default_attributes) && !in_array($prop, $this->BO->transient_attributes)) {
 				if (strtoupper(get_class($properties[$prop])) != "ENUM") {
 					echo " validation_rules[\"".$prop."\"] = ".$this->BO->$prop->get_rule().";\n";
 					echo " validation_rules[\"".$prop."_msg\"] = \"".$this->BO->$prop->get_helper()."\";\n";
