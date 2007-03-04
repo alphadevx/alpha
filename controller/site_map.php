db727a7657129ab05afe25d36a791e91a65a841c
john <alphadevx@users.noreply.github.com>
2007-03-04 01:14:45 +0000

    Added list of news items, and links are now built up using the URL property of articles and news items

--- controller/site_map.php
+++ controller/site_map.php
@@ -7,6 +7,7 @@ if(empty($sysRoot))
 require_once $sysRoot.'config/db_connect.inc';
 require_once $sysRoot.'alpha/controller/Controller.inc';
 require_once $sysRoot.'alpha/model/article_object.inc';
+require_once $sysRoot.'model/news_object.inc';
 
 /**
 * 
@@ -29,7 +30,7 @@ class site_map extends Controller
 		$this->Controller();
 		
 		$article = new article_object();
-		$article_objects = $article->load_all(0, $article->get_count());
+		$article_objects = $article->load_all(0, $article->get_count(), "date_added");
 				
 		$sections_enum = $article->get_prop_object("section");
 		$sections = $sections_enum->get_options();
@@ -43,10 +44,21 @@ class site_map extends Controller
 			echo '<ul>';
 			foreach($article_objects as $article) {
 				if($article->section->get_value() == $section && $article->published->get_value() == 1)
-					echo '<li><a href="'.$sysURL.'/alpha/controller/view_article_title.php?title='.$article->get("title").'">'.$article->get("title").'</a></li>';
+					echo '<li><a href="'.$article->URL.'">'.$article->get("title").'</a></li>';
 			}
-			echo '</ul>';			
+			echo '</ul>';
 		}
+		
+		// now list out the news items
+		$news = new news_object();
+		$news_objects = $news->load_all(0, $news->get_count(), "OID", "DESC");
+				
+		echo "<h2>News Items</h2>";
+		echo '<ul>';
+		foreach($news_objects as $news) {			
+			echo '<li><a href="'.$news->URL.'">'.$news->get("title").'</a></li>';
+		}
+		echo '</ul>';		
 		
 		$this->display_page_foot();
 	}
