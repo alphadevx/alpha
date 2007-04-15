529ba59d33b3f27ff144b1c01fa74948e82d85ad
john <alphadevx@users.noreply.github.com>
2007-04-15 22:35:11 +0000

    Removed references to time-based methods which have been moved to the new Timestamp class

--- tests/Date_Test.php
+++ tests/Date_Test.php
@@ -62,43 +62,25 @@ class Date_Test extends PHPUnit_TestCase
      * testing the constructor has set the Date to today by default
      */
     function test_default_date_value() {
-    	$this->assertEquals(date("Y-m-d"), $this->date1->get_date(), "testing the constructor has set the Date to today by default");
+    	$this->assertEquals(date("Y-m-d"), $this->date1->get_value(), "testing the constructor has set the Date to today by default");
     }
     
     /**
      * testing the set_value method
      */
     function test_set_value() {
-    	$this->date1->set_value(2000, 1, 1, 0, 0, 0);
+    	$this->date1->set_value(2000, 1, 1);
     	
-    	$this->assertEquals("2000-01-01 00:00:00", $this->date1->get_value(), "testing the set_value method");
-    }
-    
-    /**
-     * testing the set_date method
-     */
-    function test_set_date() {
-    	$this->date1->set_date(2000, 1, 1);
-    	
-    	$this->assertEquals("2000-01-01", $this->date1->get_date(), "testing the set_date method");
-    }
-    
-    /**
-     * testing the set_time method
-     */
-    function test_set_time() {
-    	$this->date1->set_time(12, 30, 5);
-    	
-    	$this->assertEquals("12:30:05", $this->date1->get_time(), "testing the set_date method");
-    }
+    	$this->assertEquals("2000-01-01", $this->date1->get_value(), "testing the set_value method");
+    }    
     
     /**
      * testing the populate_from_string method
      */
     function test_populate_from_string() {
-    	$this->date1->populate_from_string("2007-08-13 12:45:07");
+    	$this->date1->populate_from_string("2007-08-13");
     	
-    	$this->assertEquals("2007-08-13 12:45:07", $this->date1->get_value(), "testing the populate_from_string method");
+    	$this->assertEquals("2007-08-13", $this->date1->get_value(), "testing the populate_from_string method");
     }
 }
 
