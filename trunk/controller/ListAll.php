822104e27485bcbcf9920430084d2eb7f2fdf51a
john <john@1970b06b-b575-2e47-bfa4-2824af2bf091>
2008-01-01 23:42:42 +0000

    Replaced display_page_head() with after_display_page_head_callback()

--- trunk/controller/ListAll.php
+++ trunk/controller/ListAll.php
@@ -164,49 +164,12 @@ class ListAll extends Controller
 	}
 	
 	/**
-	 * method to display the page head with pageination links
+	 * Renders an administration home page link after the page header is rendered
 	 */
-	function display_page_head() {
+	function after_display_page_head_callback() {
 		global $config;
 		
-		echo '<html>';
-		echo '<head>';
-		echo '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">';
-		echo '<title>'.$this->get_title().'</title>';
-		echo '<meta name="Keywords" content="'.$this->get_keywords().'">';
-		echo '<meta name="Description" content="'.$this->get_description().'">';
-		echo '<meta name="Author" content="john collins">';
-		echo '<meta name="copyright" content="copyright ">';
-		echo '<meta name="identifier" content="http://'.$config->get('sysURL').'/">';
-		echo '<meta name="revisit-after" content="7 days">';
-		echo '<meta name="expires" content="never">';
-		echo '<meta name="language" content="en">';
-		echo '<meta name="distribution" content="global">';
-		echo '<meta name="title" content="'.$this->get_title().'">';
-		echo '<meta name="robots" content="index,follow">';
-		echo '<meta http-equiv="imagetoolbar" content="no">';			
-		
-		echo '<link rel="StyleSheet" type="text/css" href="'.$config->get('sysURL').'/config/css/'.$config->get('sysTheme').'.css.php">';
-		
-		if ($config->get('sysUseWidgets')) {
-			echo '<script language="JavaScript" src="'.$config->get('sysURL').'/alpha/scripts/addOnloadEvent.js"></script>';
-			require_once $config->get('sysRoot').'alpha/view/widgets/button.js.php';
-		}
-		
-		echo '</head>';
-		echo '<body>';
-			
-		echo '<h1>'.$this->get_title().'</h1>';
-		
-		if (isset($_SESSION["current_user"])) {	
-			echo '<p>You are logged in as '.$_SESSION["current_user"]->get_displayname().'.  <a href="'.$config->get('sysURL').'/alpha/controller/logout.php">Logout</a></p>';
-		}else{
-			echo '<p>You are not logged in</p>';
-		}
-		
 		echo '<p align="center"><a href="'.$config->get('sysURL').'/alpha/controller/ListBusinessObjects.php">Administration Home Page</a></p>';
-		
-		$this->render_page_links();
 	}
 	
 	/**
