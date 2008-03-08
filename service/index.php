9e7f5707c7c4ea3b7e4ea40e8ebdc7c9a6d2f9b4
john <alphadevx@users.noreply.github.com>
2008-03-08 20:59:06 +0000

    Modified to use secure URLs

--- service/index.php
+++ service/index.php
@@ -1,20 +1,29 @@
 <?php
+
+// $Id$
+
 /**
  *
  * Description
  * 
  * @package Alpha Admin
  * @author John Collins <john@design-ireland.net>
- * @copyright 2006 John Collins
+ * @copyright 2008 John Collins
  * 
  * 
  */
- 
-require_once '../controller/login.php';
+
+// include the config file
+if(!isset($config))
+	require_once '../util/configLoader.inc';
+$config =&configLoader::getInstance();
+
+require_once $config->get('sysRoot').'alpha/controller/login.php';
 
 $controller = new login();
-$controller->set_name('login.php');
-$controller->set_unit_of_work(array('/alpha/controller/login.php','/alpha/controller/ListBusinessObjects.php'));
+$controller->set_name(Front_Controller::generate_secure_URL('act=login'));
+
+$controller->set_unit_of_work(array(Front_Controller::generate_secure_URL('act=login'), Front_Controller::generate_secure_URL('act=ListBusinessObjects')));
 
 if(!empty($_POST)) {			
 	$controller->handle_post();		
