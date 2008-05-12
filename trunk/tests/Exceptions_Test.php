1eb910bf00bd227d7b4061944159aaf2001ba927
john <john@1970b06b-b575-2e47-bfa4-2824af2bf091>
2008-05-12 22:57:04 +0000

    Added the exceptions test suite

---
+++ trunk/tests/Exceptions_Test.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ * Test case for the exception handling functionality
+ * 
+ * @package Alpha Core Unit Tests
+ * @author John Collins <john@design-ireland.net>
+ * @copyright 2008 John Collins
+ * @version $Id$
+ * 
+ */
+class Exceptions_Test extends PHPUnit_Framework_TestCase
+{    
+    /**
+     * Testing that a division by 0 exception is caught by the general exception handler
+     */
+    public function testDivideByZeroCaught() {
+    	$exceptionCaught = false;
+    	try {
+    		2/0;
+    	}catch (AlphaPHPException $e) {
+    		$exceptionCaught = true;
+    	}
+    	
+    	$this->assertTrue($exceptionCaught, "Testing that a division by 0 exception is caught by the general exception handler");
+    }
+    
+	/**
+     * Testing that calling a property on a non-object will throw an exception
+     */
+    public function testPropertyNonObjectCaught() {
+    	$exceptionCaught = false;
+    	try {
+    		$e = $empty->test;
+    	}catch (AlphaPHPException $e) {
+    		$exceptionCaught = true;
+    	}
+    	
+    	$this->assertTrue($exceptionCaught, "Testing that calling a property on a non-object will throw an exception");
+    }
+}
+
+?>
