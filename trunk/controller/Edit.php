4e4de430e962999f0f42872f8c0d03183ebce24c
johnc <johnc@1970b06b-b575-2e47-bfa4-2824af2bf091>
2009-01-03 17:28:08 +0000

    Updated the Edit controller to PHP5 and new controller interface standards

--- trunk/controller/Edit.php
+++ trunk/controller/Edit.php
@@ -1,7 +1,5 @@
 <?php
 
-// $Id$
-
 // include the config file
 if(!isset($config))
 	require_once '../util/configLoader.inc';
@@ -10,177 +8,211 @@ $config =&configLoader::getInstance();
 require_once $config->get('sysRoot').'alpha/util/db_connect.inc';
 require_once $config->get('sysRoot').'alpha/controller/Controller.inc';
 require_once $config->get('sysRoot').'alpha/view/View.inc';
+require_once $config->get('sysRoot').'alpha/controller/AlphaControllerInterface.inc';
 
 /**
-* 
-* Controller used to edit BO, which must be supplied in GET vars
-* 
-* @package Alpha Core Scaffolding
-* @author John Collins <john@design-ireland.net>
-* @copyright 2008 John Collins
-*
-*/
-class Edit extends Controller
-{
+ * 
+ * Controller used to edit BO, which must be supplied in GET vars
+ * 
+ * @package alpha::controller
+ * @author John Collins <john@design-ireland.net>
+ * @copyright 2009 John Collins
+ * @version $Id$
+ *
+ */
+class Edit extends Controller implements AlphaControllerInterface {
 	/**
-	 * the new to be edited
-	 * @var object BO
+	 * The business object to be edited
+	 * 
+	 * @var Object
 	 */
-	var $BO;
+	protected $BO;
 	
 	/**
-	 * the name of the BO
+	 * The name of the BO
+	 * 
 	 * @var string
 	 */
-	var $BO_name;
+	private $BOName;
 	
 	/**
-	 * the OID of the BO to be loaded
+	 * The OID of the BO to be edited
+	 * 
 	 * @var int
 	 */
-	var $BO_oid;
+	private $BOoid;
 	
 	/**
-	 * the new default View object used for rendering the object to edit
-	 * @var View BO_view
+	 * The View object used for rendering the object to edit
+	 * 
+	 * @var View BOView
 	 */
-	var $BO_View;
+	private $BOView;
 								
 	/**
-	 * constructor that renders the page	
+	 * Trace logger
+	 * 
+	 * @var Logger
 	 */
-	function Edit() {
+	private static $logger = null;
+								
+	/**
+	 * constructor to set up the object
+	 */
+	public function __construct() {
+		if(self::$logger == null)
+			self::$logger = new Logger('Edit');
+		self::$logger->debug('>>__construct()');
+		
 		global $config;
 		
-		// load the business object (BO) definition
-		if (isset($_GET["bo"])) {
-			$BO_name = $_GET["bo"];
-			if (file_exists($config->get('sysRoot').'alpha/model/'.$BO_name.'.inc')) {
-				require_once $config->get('sysRoot').'alpha/model/'.$BO_name.'.inc';
-			}elseif (file_exists($config->get('sysRoot').'model/'.$BO_name.'.inc')) {
-				require_once $config->get('sysRoot').'model/'.$BO_name.'.inc';
+		// ensure that the super class constructor is called, indicating the rights group
+		parent::__construct('Admin');
+		
+		self::$logger->debug('<<__construct');
+	}
+	
+	/**
+	 * Handle GET requests
+	 * 
+	 * @param array $params
+	 */
+	public function doGET($params) {
+		try{
+			// load the business object (BO) definition
+			if (isset($params['bo']) && isset($params['oid'])) {
+				$BOName = $params['bo'];
+				DAO::loadClassDef($BOName);
+				
+				/*
+				 *  check and see if a custom create controller exists for this BO, and if it does use it otherwise continue
+				 */
+				$this->loadCustomController($BOName, 'edit');
+				
+				$this->BO = new $BOName();
+				$this->BO->load($params['oid']);
+				
+				$this->BOName = $BOName;
+				
+				$this->BOView = View::getInstance($this->BO);
+				
+				// set up the title and meta details
+				$this->setTitle('Editing a '.$BOName);
+				$this->setDescription('Page to edit a '.$BOName.'.');
+				$this->setKeywords('edit,'.$BOName);
+				
+				echo View::displayPageHead($this);
+		
+				echo View::renderDeleteForm();
+		
+				echo $this->BOView->editView();		
 			}else{
-				$error = new handle_error($_SERVER["PHP_SELF"],'Could not load the defination for the BO class '.$BO_name,'GET');
-				exit;
+				throw new IllegalArguementException('No BO available to edit!');
 			}
-		}else{
-			$error = new handle_error($_SERVER["PHP_SELF"],'No BO available to edit!','GET');
-			exit;
+		}catch(IllegalArguementException $e) {
+			self::$logger->error($e->getMessage());
+		}catch(BONotFoundException $e) {
+			self::$logger->warn($e->getMessage());
+			echo '<p class="error"><br>Failed to load the requested item from the database!</p>';
 		}
 		
-		// ensure that a OID is also provided
-		if (isset($_GET["oid"])) {
-			$BO_oid = $_GET["oid"];
-		}else{
-			$error = new handle_error($_SERVER["PHP_SELF"],'Could not load the BO object '.$BO_name.' as an oid was not supplied!','GET');
-			exit;
-		}
-		
-		// check and see if a custom edit_*.php controller exists for this BO, and if it does use it otherwise continue
-		if (file_exists($config->get('sysRoot').'controller/edit_'.$BO_name.'.php')) {
-			// handle secure URLs
-			if(isset($_GET['tk']))
-				header('Location: '.Front_Controller::generate_secure_URL('act=edit_'.$BO_name.'&'.Front_Controller::decode_query_params($_SERVER['QUERY_STRING'])));
-			else
-				header('Location: '.$config->get('sysURL').'/controller/edit_'.$BO_name.'.php?'.$_SERVER['QUERY_STRING']);
-		}
-		if (file_exists($config->get('sysRoot').'alpha/controller/edit_'.$BO_name.'.php')) {
-			// handle secure URLs
-			if(isset($_GET['tk']))
-				header('Location: '.Front_Controller::generate_secure_URL('act=edit_'.$BO_name.'&'.Front_Controller::decode_query_params($_SERVER['QUERY_STRING'])));
-			else
-				header('Location: '.$config->get('sysURL').'/alpha/controller/edit_'.$BO_name.'.php?'.$_SERVER['QUERY_STRING']);	
-		}
-		
-		// ensure that the super class constructor is called
-		$this->Controller();
-		
-		$this->BO = new $BO_name();
-		$this->BO->load($BO_oid);
-		
-		$this->BO_name = $BO_name;
-		
-		$this->BO_View = View::getInstance($this->BO);
-		
-		// set up the title and meta details
-		$this->set_title("Editing a ".$BO_name);
-		$this->set_description("Page to edit a ".$BO_name.".");
-		$this->set_keywords("edit,".$BO_name);
-		
-		$this->set_visibility('Administrator');
-		if(!$this->check_rights()) {
-			exit;
-		}	
-		
-		$this->display_page_head();
-		
-		$this->render_delete_form();
-		
-		if(!empty($_POST)) {
-			$this->handle_post();
-			return;
-		}
-		
-		$this->BO_View->editView();		
-		
-		$this->display_page_foot();
+		echo View::displayPageFoot($this);
 	}
 	
 	/**
 	 * method to handle POST requests
 	 */
-	function handle_post() {
+	function doPOST($params) {
 		global $config;
 		
-		// check the hidden security fields before accepting the form POST data
-		if(!$this->check_security_fields()) {
-			$error = new handle_error($_SERVER["PHP_SELF"],'This page cannot accept post data from remote servers!','handle_post()','validation');
-			exit;
-		}
-		
-		if (isset($_POST["saveBut"])) {			
-			
-			// populate the transient object from post data
-			$this->BO->populateFromPost();
-			
-			try {
-				$success = $this->BO->save();			
-				echo '<p class="success">'.get_class($this->BO).' '.$this->BO->getID().' saved successfully.</p>';
-			}catch (LockingException $e) {
-				$this->BO->reload();
+		try {
+			// check the hidden security fields before accepting the form POST data
+			if(!$this->checkSecurityFields()) {
+				throw new SecurityException('This page cannot accept post data from remote servers!');
+				self::$logger->debug('<<doPOST');
 			}
 			
-			$this->BO_View->setBO($this->BO);
-			
-			$this->BO_View->editView();		
-		
-			$this->display_page_foot();
-		}
-		
-		if (!empty($_POST["delete_oid"])) {
-			
-			$temp = new $this->BO_name();
-			
-			$temp->load($_POST["delete_oid"]);			
-					
-			try {
-				$success = $temp->delete();			
-				echo '<p class="success">'.$this->BO_name.' '.$_POST["delete_oid"].' deleted successfully.</p>';
+			// load the business object (BO) definition
+			if (isset($params['bo']) && isset($params['oid'])) {
+				$BOName = $params['bo'];
+				DAO::loadClassDef($BOName);
 				
-				echo '<center>';
-				$temp = new button("document.location = '".$config->get('sysURL')."/alpha/controller/ListAll.php?bo=".get_class($this->BO)."'","Back to List","cancelBut");
-				echo $temp->render();
-				echo '</center>';
-			}catch (FailedDeleteException $e) {
-				echo '<p class="error">'.$this->BO_name.' '.$_POST["delete_oid"].' deleted failed, database rolled back.</p>';
+				$this->BO = new $BOName();
+				$this->BO->load($params['oid']);
+				
+				$this->BOView = View::getInstance($this->BO);
+				
+				$this->BOView->setBO($this->BO);
+					
+				// set up the title and meta details
+				$this->setTitle('Editing a '.$BOName);
+				$this->setDescription('Page to edit a '.$BOName.'.');
+				$this->setKeywords('edit,'.$BOName);
+					
+				echo View::displayPageHead($this);
+		
+				if (isset($params['saveBut'])) {			
+					
+					// populate the transient object from post data
+					$this->BO->populateFromPost();
+					
+					try {
+						$success = $this->BO->save();			
+						echo '<p class="success">'.get_class($this->BO).' '.$this->BO->getID().' saved successfully.</p>';
+					}catch (LockingException $e) {
+						$this->BO->reload();
+						echo '<p class="error"><br>'.$e->getMessage().'</p>';
+					}
+					
+					$this->BOView->editView();
+				}
+				
+				if (!empty($params['delete_oid'])) {
+					$temp = new $BOName();
+					$temp->load($params['delete_oid']);
+					
+					try {
+						$temp->delete();
+								
+						echo '<p class="success">'.$this->BOName.' '.$params['delete_oid'].' deleted successfully.</p>';
+										
+						echo '<center>';
+						
+						$temp = new button("document.location = '".Front_Controller::generate_secure_URL('act=ListAll&bo='.get_class($this->BO))."'",'Back to List','cancelBut');
+						echo $temp->render();
+						
+						echo '</center>';
+					}catch(AlphaException $e) {
+						self::$logger->error($e->getTraceAsString());
+						echo '<p class="error"><br>Error deleting the OID ['.$params['delete_oid'].'], check the log!</p>';
+					}
+				}
+			}else{
+				throw new IllegalArguementException('No BO available to edit!');
 			}
-			exit;
+		}catch(SecurityException $e) {
+			echo '<p class="error"><br>'.$e->getMessage().'</p>';								
+			self::$logger->warn($e->getMessage());
+		}catch(IllegalArguementException $e) {
+			self::$logger->error($e->getMessage());
+		}catch(BONotFoundException $e) {
+			self::$logger->warn($e->getMessage());
+			echo '<p class="error"><br>Failed to load the requested item from the database!</p>';
 		}
-	}	
+		
+		echo View::displayPageFoot($this);
+	}
 }
 
 // now build the new controller
-if(basename($_SERVER["PHP_SELF"]) == "Edit.php")
+if(basename($_SERVER['PHP_SELF']) == 'Edit.php') {
 	$controller = new Edit();
+	
+	if(!empty($_POST)) {			
+		$controller->doPOST($_REQUEST);
+	}else{
+		$controller->doGET($_GET);
+	}
+}
 
 ?>
