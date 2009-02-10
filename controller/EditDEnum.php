a0160578ffca8b6b1c7b4e9ec90221d1c5b18104
johnc <alphadevx@users.noreply.github.com>
2009-02-10 22:49:14 +0000

    Upgraded the EditDEnum controller to PHP5 and new controller interface

--- controller/EditDEnum.php
+++ controller/EditDEnum.php
@@ -1,7 +1,5 @@
 <?php
 
-// $Id$
-
 // include the config file
 if(!isset($config))
 	require_once '../util/configLoader.inc';
@@ -11,133 +9,203 @@ require_once $config->get('sysRoot').'alpha/controller/Edit.php';
 require_once $config->get('sysRoot').'alpha/model/types/DEnum.inc';
 require_once $config->get('sysRoot').'alpha/model/types/DEnumItem.inc';
 require_once $config->get('sysRoot').'alpha/view/DEnumView.inc';
+require_once $config->get('sysRoot').'alpha/controller/AlphaControllerInterface.inc';
+require_once $config->get('sysRoot').'alpha/exceptions/IllegalArguementException.inc';
+require_once $config->get('sysRoot').'alpha/exceptions/BONotFoundException.inc';
+require_once $config->get('sysRoot').'alpha/exceptions/FailedSaveException.inc';
 
 /**
-* 
-* Controller used to edit DEnums and associated DEnumItems
-* 
-* @package Alpha Core Scaffolding
-* @author John Collins <john@design-ireland.net>
-* @copyright 2008 John Collins
-*
-*/
-class EditDEnum extends Edit
-{
+ * 
+ * Controller used to edit DEnums and associated DEnumItems
+ * 
+ * @package alpha::controller
+ * @author John Collins <john@design-ireland.net>
+ * @copyright 2009 John Collins
+ * @version $Id$
+ * 
+ */
+class EditDEnum extends Edit implements AlphaControllerInterface {
 	/**
-	 * constructor that renders the page	
+	 * Used to set status update messages to display to the user
+	 *
+	 * @var string
 	 */
-	function EditDEnum() {
-		global $config;
+	private $statusMessage = '';
+	
+	/**
+	 * Trace logger
+	 * 
+	 * @var Logger
+	 */
+	private static $logger = null;
+	
+	/**
+	 * constructor to set up the object
+	 */
+	public function __construct() {
+		if(self::$logger == null)
+			self::$logger = new Logger('ListDEnums');
+		self::$logger->debug('>>__construct()');
 		
-		// ensure that a OID is provided
-		if (isset($_GET["oid"])) {
-			$BO_oid = $_GET["oid"];
-		}else{
-			$error = new handle_error($_SERVER["PHP_SELF"],'Could not load the DEnum object as an oid was not supplied!','GET');
-			exit;
-		}
-		
-		// ensure that the super class constructor is called
-		$this->Controller();
-		
-		$this->BO = new DEnum();
-		$this->BO->load($BO_oid);
-		
-		$this->BO_name = "DEnum";
-		
-		$this->BO_View = new DEnumView($this->BO);
+		// ensure that the super class constructor is called, indicating the rights group
+		parent::__construct('Admin');
 		
 		// set up the title and meta details
-		$this->set_title("Editing a DEnum");
-		$this->set_description("Page to edit a DEnum.");
-		$this->set_keywords("edit,DEnum");
+		$this->setTitle('Editing a DEnum');
+		$this->setDescription('Page to edit a DEnum.');
+		$this->setKeywords('edit,DEnum');
 		
-		$this->set_visibility('Administrator');
-		if(!$this->check_rights()) {
-			exit;
-		}	
+		$this->BO = new DEnum();
 		
-		$this->display_page_head();
-		
-		$this->render_delete_form();
-		
-		if(!empty($_POST)) {
-			$this->handle_post();
-			return;
-		}
-		
-		$this->BO_View->edit_view();		
-		
-		$this->display_page_foot();
+		self::$logger->debug('<<__construct');
 	}
 	
 	/**
-	 * method to handle POST requests
+	 * Handle GET requests
+	 * 
+	 * @param array $params
 	 */
-	function handle_post() {
+	public function doGET($params) {
+		self::$logger->debug('>>doGET($params=['.print_r($params, true).'])');
+		
 		global $config;
 		
-		// check the hidden security fields before accepting the form POST data
-		if(!$this->check_security_fields()) {
-			$error = new handle_error($_SERVER["PHP_SELF"],'This page cannot accept post data from remote servers!','handle_post()','validation');
-			exit;
+		echo View::displayPageHead($this);
+		
+		// ensure that a OID is provided
+		if (isset($params['oid'])) {
+			$BOoid = $params['oid'];
+		}else{
+			throw new IllegalArguementException('Could not load the DEnum object as an oid was not supplied!');
+			return;
 		}
 		
-		if (isset($_POST["saveBut"])) {			
+		try {
+			$this->BO->load($BOoid);
 			
-			// populate the transient object from post data
-			$this->BO->populateFromPost();
+			$this->BOName = 'DEnum';
 			
-			$success = $this->BO->save();			
+			$this->BOView = new DEnumView($this->BO);			
 			
-			$this->BO->load($this->BO->getID());
+			echo View::renderDeleteForm();			
 			
-			// now save the DEnumItems			
-			$tmp = new DEnumItem();
-			$denumItems = $tmp->loadItems($this->BO->getID());						
-			
-			foreach ($denumItems as $item) {
-				$item->set("value", $_POST["value_".$item->getID()]);
-				$this->mark_dirty($item);
-			}
-			
-			// handle new DEnumItem if posted
-			if(isset($_POST["new_value"]) && trim($_POST["new_value"]) != "") {
-				$newItem = new DEnumItem();
-				$newItem->set("value", $_POST["new_value"]);
-				$newItem->set("DEnumID", $this->BO->getID());
-				$this->markNew($newItem);
-			}			
-					
-			$success = $this->commit();
-			
-			if($success) {
-				echo '<p class="success">'.get_class($this->BO).' '.$this->BO->getID().' saved successfully.</p>';
-			}
-			
-			$this->BO_View->setBO($this->BO);
-			
-			$this->BO_View->edit_view();		
+			echo $this->BOView->editView();
+		}catch(BONotFoundException $e) {
+			self::$logger->error('Unable to load the DEnum of id ['.$params['oid'].'], error was ['.$e->getMessage().']');
+		}
 		
-			$this->display_page_foot();
-		}		
+		echo View::displayPageFoot($this);
+		
+		self::$logger->debug('<<doGET');		
+	}
+	
+	/**
+	 * Handle POST requests (adds $currentUser person_object to the session)
+	 * 
+	 * @param array $params
+	 */
+	public function doPOST($params) {
+		self::$logger->debug('>>doPOST($params=['.print_r($params, true).'])');
+		
+		try {
+			// check the hidden security fields before accepting the form POST data
+			if(!$this->checkSecurityFields()) {
+				throw new SecurityException('This page cannot accept post data from remote servers!');
+				self::$logger->debug('<<doPOST');
+			}
+		
+			// ensure that a OID is provided
+			if (isset($params['oid'])) {
+				$BOoid = $params['oid'];
+			}else{
+				throw new IllegalArguementException('Could not load the DEnum object as an oid was not supplied!');
+			}
+			
+			if (isset($params['saveBut'])) {			
+				try {
+					$this->BO->load($BOoid);
+					// update the object from post data
+					$this->BO->populateFromPost();
+					$this->BO->save();
+					
+					// now save the DEnumItems			
+					$tmp = new DEnumItem();
+					$denumItems = $tmp->loadItems($this->BO->getID());						
+					
+					foreach ($denumItems as $item) {
+						$item->set('value', $params['value_'.$item->getID()]);
+						$this->markDirty($item);
+					}
+					
+					// handle new DEnumItem if posted
+					if(isset($params['new_value']) && trim($params['new_value']) != '') {
+						$newItem = new DEnumItem();
+						$newItem->set('value', $params['new_value']);
+						$newItem->set('DEnumID', $this->BO->getID());
+						$this->markNew($newItem);
+					}			
+							
+					$this->commit();
+					
+					$this->statusMessage = '<p class="success">'.get_class($this->BO).' '.$this->BO->getID().' saved successfully.</p>';
+					
+					$this->doGET($params);
+				}catch (FailedSaveException $e) {
+					self::$logger->error('Unable to save the DEnum of id ['.$params['oid'].'], error was ['.$e->getMessage().']');
+					$this->abort();
+				}
+			}
+		}catch(SecurityException $e) {
+			echo '<p class="error"><br>'.$e->getMessage().'</p>';								
+			self::$logger->warn($e->getMessage());
+		}catch(IllegalArguementException $e) {
+			self::$logger->error($e->getMessage());
+		}catch(BONotFoundException $e) {
+			self::$logger->warn($e->getMessage());
+			echo '<p class="error"><br>Failed to load the requested item from the database!</p>';
+		}
+				
+		self::$logger->debug('<<doPOST');		
 	}
 	
 	/**
 	 * Using this callback to blank the new_value field when the page loads, regardless of anything being posted
+	 * 
+	 * @return string
 	 */
-	function during_display_page_head_callback() {
-		echo '<script language="javascript">';
-		echo 'function clearNewField() {';
-		echo '	document.getElementById("new_value").value = "";';
-		echo '}';
-		echo 'addOnloadEvent(clearNewField);';
-		echo '</script>';	
+	public function during_displayPageHead_callback() {
+		$html = '';
+		$html .= '<script language="javascript">';
+		$html .= 'function clearNewField() {';
+		$html .= '	document.getElementById("new_value").value = "";';
+		$html .= '}';
+		$html .= 'addOnloadEvent(clearNewField);';
+		$html .= '</script>';
+		return $html;
 	}
+	
+	/**
+	 * Callback used to render a status message if there is one
+	 *
+	 * @return string
+	 */
+	public function after_displayPageHead_callback() {
+		$html = '';
+		if($this->statusMessage != '')
+			$html .= $this->statusMessage;
+		return $html;
+	}	
 }
 
-// now build the new controller
-if(basename($_SERVER["PHP_SELF"]) == "EditDEnum.php")
+// now build the new controller if this file is called directly
+if ('EditDEnum.php' == basename($_SERVER['PHP_SELF'])) {
 	$controller = new EditDEnum();
+	
+	if(!empty($_POST)) {			
+		$controller->doPOST($_REQUEST);
+	}else{
+		$controller->doGET($_GET);
+	}
+}
 
 ?>
