0bfad8dc889172c33f40da90d1a01c0154e4cb85
johnc <johnc@1970b06b-b575-2e47-bfa4-2824af2bf091>
2009-03-29 14:04:49 +0000

    Upgrading feeds to PHP5

--- trunk/controller/ViewFeed.php
+++ trunk/controller/ViewFeed.php
@@ -5,6 +5,7 @@ if(!isset($config))
 	require_once '../util/configLoader.inc';
 $config =&configLoader::getInstance();
 
+require_once $config->get('sysRoot').'alpha/util/db_connect.inc';
 require_once $config->get('sysRoot').'alpha/controller/Controller.inc';
 require_once $config->get('sysRoot').'alpha/util/feeds/RSS2.inc';
 require_once $config->get('sysRoot').'alpha/util/feeds/RSS.inc';
