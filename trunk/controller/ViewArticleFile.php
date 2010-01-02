65d99fc4a8ffa065eb6f59dcd1d82cd7df087b08
johnc <johnc@1970b06b-b575-2e47-bfa4-2824af2bf091>
2010-01-02 17:06:10 +0000

    Fixed a bug when supplying an absolute path to a file to the VuewArticleFile controller

--- trunk/controller/ViewArticleFile.php
+++ trunk/controller/ViewArticleFile.php
@@ -20,19 +20,30 @@ require_once $config->get('sysRoot').'alpha/exceptions/FileNotFoundException.inc
  * @version $Id$
  *
  */
-class ViewArticleFile extends ViewArticle {								
+class ViewArticleFile extends ViewArticle {
+	/**
+	 * Trace logger
+	 * 
+	 * @var Logger
+	 */
+	private static $logger = null;
+									
 	/**
 	 * Handle GET requests
 	 * 
 	 * @param array $params
 	 */
 	public function doGET($params) {
+		if(self::$logger == null)
+			self::$logger = new Logger('ViewArticleFile');
+			
 		global $config;
 		
 		try {
 			// ensure that a title is provided
 			if (isset($params['file'])) {
-				$title = str_replace('_', ' ', $params['file']);
+				$title = basename($params['file']);
+				$title = str_replace('_', ' ', $title);
 				$title = str_replace('.text', '', $title);
 			}else{
 				throw new IllegalArguementException('Could not load the article as a file name was not supplied!');
@@ -40,7 +51,11 @@ class ViewArticleFile extends ViewArticle {
 			
 			$this->BO = new article_object();
 			$this->BO->set('title', $title);
-			$this->BO->loadContentFromFile($config->get('sysRoot').'/alpha/docs/'.$params['file']);
+			// just checking to see if the file path is absolute or not
+			if(substr($params['file'], 0, 1) == '/')
+				$this->BO->loadContentFromFile($params['file']);
+			else
+				$this->BO->loadContentFromFile($config->get('sysRoot').'/alpha/docs/'.$params['file']);
 			
 		}catch(IllegalArguementException $e) {
 			self::$logger->error($e->getMessage());
