4e8136c9b12e354900d0e4400871f5a07c9be371
johnc <johnc@1970b06b-b575-2e47-bfa4-2824af2bf091>
2010-01-03 11:13:35 +0000

    Renamed gen_secure_query_strings to GenSecureQueryStrings and upgraded to PHP5

---
+++ trunk/controller/GenSecureQueryStrings.php
@@ -0,0 +1,107 @@
+<?php
+
+// 
+
+// include the config file
+if(!isset($config)) {
+	require_once '../util/AlphaConfig.inc';
+	$config = AlphaConfig::getInstance();
+}
+
+require_once $config->get('sysRoot').'alpha/controller/Controller.inc';
+require_once $config->get('sysRoot').'alpha/controller/front/FrontController.inc';
+require_once $config->get('sysRoot').'alpha/controller/AlphaControllerInterface.inc';
+require_once $config->get('sysRoot').'alpha/view/View.inc';
+
+/**
+ *
+ * Controller used to generate secure URLs from the query strings provided
+ * 
+ * @package alpha::controller
+ * @author John Collins <john@design-ireland.net>
+ * @copyright 2010 John Collins
+ * @version $Id$
+ */
+class GenSecureQueryStrings extends Controller implements AlphaControllerInterface {
+	/**
+	 * Trace logger
+	 * 
+	 * @var Logger
+	 */
+	private static $logger = null;
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		if(self::$logger == null)
+			self::$logger = new Logger('CacheManager');
+		self::$logger->debug('>>__construct()');
+		
+		global $config;
+		
+		// ensure that the super class constructor is called, indicating the rights group
+		parent::__construct('Admin');
+		
+		$this->setTitle('Generate Secure Query Strings');
+		
+		self::$logger->debug('<<__construct');
+	}
+	
+	/**
+	 * Handle GET requests
+	 * 
+	 * @param array $params
+	 */
+	public function doGET($params) {
+		echo View::displayPageHead($this);
+		
+		$this->renderForm();
+		
+		echo View::displayPageFoot($this);
+	}
+	
+	/**
+	 * Handle POST requests (adds $currentUser person_object to the session)
+	 * 
+	 * @param array $params
+	 */
+	public function doPOST($params) {
+		global $config;
+
+		echo View::displayPageHead($this);
+		
+		echo '<p style="width:90%; overflow:scroll;">';
+		if(isset($params['QS']))
+			echo $config->get('sysURL')."FC.php?tk=".FrontController::encodeQuery($params['QS']);
+		echo '</p>';
+		
+		$this->renderForm();
+		
+		echo View::displayPageFoot($this);
+	}
+	
+	private function renderForm() {
+		echo '<p>Use this form to generate secure (encrypted) URLs which make use of the Front Controller.  Always be sure to specify an action controller (act) at a minimum.</p>';
+		echo '<p>Example 1: to generate a secure URL for viewing article object 00000000001, enter <em>act=ViewArticle&oid=00000000001</em></p>';
+		echo '<p>Example 2: to generate a secure URL for viewing an Atom news feed of the articles, enter <em>act=ViewFeed&bo=article_object&type=Atom</em</p>';
+
+		echo '<form action="'.$_SERVER["PHP_SELF"].(empty($_SERVER["QUERY_STRING"])? '':'?'.$_SERVER["QUERY_STRING"]).'" method="post">';
+		echo '<input type="text" name="QS" size="100"/>';
+		echo '<input type="submit" value="Generate"/>';
+		echo '</form>';
+	}
+}
+
+// now build the new controller if this file is called directly
+if ('GenSecureQueryStrings.php' == basename($_SERVER['PHP_SELF'])) {
+	$controller = new GenSecureQueryStrings();
+	
+	if(!empty($_POST)) {			
+		$controller->doPOST($_QUERY);
+	}else{
+		$controller->doGET($_GET);
+	}
+}
+
+?>
