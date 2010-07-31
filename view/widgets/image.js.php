28740ba6b22814e422c27cc409eec69f9e911d08
jcollins <alphadevx@users.noreply.github.com>
2010-07-31 17:01:04 +0000

    Removing usages of the depreciated configLoader class

--- view/widgets/image.js.php
+++ view/widgets/image.js.php
@@ -2,9 +2,10 @@
 
 // $Id$
 
-if(!isset($config))
-	require_once '../../util/configLoader.inc';
-$config =&configLoader::getInstance();
+if(!isset($config)) {
+	require_once '../../util/AlphaConfig.inc';
+	$config = AlphaConfig::getInstance();
+}
 
 require_once $config->get('sysRoot').'alpha/util/handle_error.inc';
 require_once $config->get('sysRoot').'alpha/model/types/String.inc';
