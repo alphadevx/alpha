5251680d494d50f74381924aa3a7688c42b732e9
johnc <johnc@1970b06b-b575-2e47-bfa4-2824af2bf091>
2010-12-28 18:38:25 +0000

    Adding more FrontController unit tests

--- trunk/tests/FrontController_Test.php
+++ trunk/tests/FrontController_Test.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once $config->get('sysRoot').'alpha/controller/front/FrontController.inc';
+require_once $config->get('sysRoot').'alpha/util/filters/ClientBlacklistFilter.inc';
 
 /**
  *
@@ -97,6 +98,126 @@ class FrontController_Test extends PHPUnit_Framework_TestCase {
     	$front = new FrontController();
     	
     	$this->assertEquals('ViewArticleTitle', $front->getPageController(), 'testing that the constructor can parse the correct page controller action from an encrypted token param provided on a mod-rewrite style URL');
+    }
+    
+    /**
+     * testing the encodeQuery method with a known encrypted result for a test key
+     */
+    public function testEncodeQuery() {
+    	global $config;
+    	
+    	$oldKey = $config->get('sysQSKey');
+    	$config->set('sysQSKey', 'testkey');
+    	$params = 'act=ViewArticleTitle&title=Test_Title';
+    	
+    	$this->assertEquals(FrontController::encodeQuery($params), '8kqoeebEej0V-FN5-DOdA1HBDDieFcNWTib2yLSUNjq0B0FWzAupIA==', 'testing the encodeQuery method with a known encrypted result for a test key');
+    	
+    	$config->set('sysQSKey', $oldKey);
+    }
+    
+    /**
+     * testing the decodeQueryParams method with a known encrypted result for a test key
+     */
+    public function testDecodeQueryParams() {
+    	global $config;
+    	
+    	$oldKey = $config->get('sysQSKey');
+    	$config->set('sysQSKey', 'testkey');
+    	$tk = '8kqoeebEej0V-FN5-DOdA1HBDDieFcNWTib2yLSUNjq0B0FWzAupIA==';
+    	
+    	$this->assertEquals('act=ViewArticleTitle&title=Test_Title', FrontController::decodeQueryParams($tk), 'testing the decodeQueryParams method with a known encrypted result for a test key');
+    }
+    
+    /**
+     * testing that the getDecodeQueryParams method will return the known params with a known encrypted result for a test key
+     */
+    public function testGetDecodeQueryParams() {
+    	global $config;
+    	
+    	$oldKey = $config->get('sysQSKey');
+    	$config->set('sysQSKey', 'testkey');
+    	$tk = '8kqoeebEej0V-FN5-DOdA1HBDDieFcNWTib2yLSUNjq0B0FWzAupIA==';
+    	
+    	$decoded = FrontController::getDecodeQueryParams($tk);
+    	
+    	$this->assertEquals('ViewArticleTitle', $decoded['act'], 'testing that the getDecodeQueryParams method will return the known params with a known encrypted result for a test key');
+    	$this->assertEquals('Test_Title', $decoded['title'], 'testing that the getDecodeQueryParams method will return the known params with a known encrypted result for a test key');
+    }
+    
+    /**
+     * testing that a request to a bad URL will result in a ResourceNotFoundException exception
+     */
+    public function testLoadControllerFileNotFound() {
+    	global $config;
+    	
+    	$request = $config->get('sysURL').'doesNotExists';
+    	$_SERVER['REQUEST_URI'] = str_replace('http://'.$_SERVER['HTTP_HOST'], '', $request);
+    	$front = new FrontController();
+    	
+    	try{
+    		$front->loadController(false);
+    		$this->fail('testing that a request to a bad URL will result in a ResourceNotFoundException exception');
+    	}catch (ResourceNotFoundException $e) {
+    		$this->assertTrue($e->getMessage() != '', 'testing that a request to a bad URL will result in a ResourceNotFoundException exception');
+    	}
+    }
+    
+    /**
+     * testing the setting up and checking for the existence of a controller alias
+     */
+    public function testDefineAlias() {
+    	$front = new FrontController();
+    	$front->registerAlias('ViewArticleTitle','article','title');
+    	
+    	$this->assertTrue($front->hasAlias('ViewArticleTitle'), 'testing the setting up and checking for the existence of a controller alias');
+    	$this->assertTrue($front->checkAlias('article'), 'testing the setting up and checking for the existence of a controller alias');
+    	$this->assertEquals('ViewArticleTitle', $front->getAliasController('article'), 'testing the setting up and checking for the existence of a controller alias');
+    	$this->assertEquals('article', $front->getControllerAlias('ViewArticleTitle'), 'testing the setting up and checking for the existence of a controller alias');
+    }
+    
+    /**
+     * testing the accessing of the expected param for a given alias/controller
+     */
+    public function testAccessingAliasParamNames() {
+    	$front = new FrontController();
+    	$front->registerAlias('ViewArticleTitle','article','title');
+    	
+    	$this->assertEquals('title', $front->getAliasParam('article'), 'testing the accessing of the expected param for a given alias/controller');
+    	$this->assertEquals('title', $front->getControllerParam('ViewArticleTitle'), 'testing the accessing of the expected param for a given alias/controller');
+    }
+    
+    /**
+     * testing the registerFilter method with a valid filter object
+     */
+    public function testRegisterFilterGood() {
+    	try {
+    		$front = new FrontController();
+    		$front->registerFilter(new ClientBlacklistFilter());
+    		
+    		$found = false;
+    		
+    		foreach ($front->getFilters() as $filter) {
+    			if($filter instanceof ClientBlacklistFilter)
+    				$found = true;
+    		}
+    		$this->assertTrue($found, 'testing the registerFilter method with a valid filter object');
+    	}catch (IllegalArguementException $e) {
+    		$this->fail('testing the registerFilter method with a valid filter object');
+    	}
+    }
+    
+	/**
+     * testing the registerFilter method with a bad filter object
+     */
+    public function testRegisterFilterBad() {
+    	try {
+    		$front = new FrontController();
+    		$front->registerFilter(new FrontController());
+    		
+    		$this->fail('testing the registerFilter method with a bad filter object');
+    	}catch (IllegalArguementException $e) {
+    		$this->assertEquals('Supplied filter object is not a valid AlphaFilterInterface instance!', $e->getMessage(), 'testing the registerFilter method with a bad filter object');
+    	}
     }
 }
 
