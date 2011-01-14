48d56778c1e21d92f49fa645debf3a7bc3e87a39
johnc <johnc@1970b06b-b575-2e47-bfa4-2824af2bf091>
2011-01-14 12:16:11 +0000

    Added PHPDoc and tweeks to the Logout controller

--- trunk/controller/Logout.php
+++ trunk/controller/Logout.php
@@ -8,32 +8,67 @@ if(!isset($config)) {
 
 require_once $config->get('sysRoot').'alpha/util/Logger.inc';
 require_once $config->get('sysRoot').'alpha/model/person_object.inc';
-require_once $config->get('sysRoot').'alpha/view/person.inc';
-require_once $config->get('sysRoot').'alpha/util/db_connect.inc';
 require_once $config->get('sysRoot').'alpha/controller/AlphaController.inc';
 require_once $config->get('sysRoot').'alpha/controller/AlphaControllerInterface.inc';
 
 /**
  *
- * Logout controller that removes the current user object to the session
+ * Logout controller that removes the current user object from the session
  * 
- * @package Alpha Admin
+ * @package alpha::controller
+ * @since 1.0
  * @author John Collins <john@design-ireland.net>
- * @copyright 2006 John Collins
- * @todo logging of user Logout times
  * @version $Id$
+ * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @copyright Copyright (c) 2010, John Collins (founder of Alpha Framework).  
+ * All rights reserved.
  * 
+ * <pre>
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above 
+ *   copyright notice, this list of conditions and the 
+ *   following disclaimer.
+ * * Redistributions in binary form must reproduce the above 
+ *   copyright notice, this list of conditions and the 
+ *   following disclaimer in the documentation and/or other 
+ *   materials provided with the distribution.
+ * * Neither the name of the Alpha Framework nor the names 
+ *   of its contributors may be used to endorse or promote 
+ *   products derived from this software without specific 
+ *   prior written permission.
+ *   
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * </pre>
+ *  
  */
 class Logout extends AlphaController implements AlphaControllerInterface {
 	/**
 	 * Trace logger
 	 * 
 	 * @var Logger
+	 * @since 1.0
 	 */
 	private static $logger = null;
 	
 	/**
 	 * constructor to set up the object
+	 * 
+	 * @since 1.0
 	 */
 	public function __construct() {
 		self::$logger = new Logger('Logout');
@@ -42,7 +77,10 @@ class Logout extends AlphaController implements AlphaControllerInterface {
 		// ensure that the super class constructor is called, indicating the rights group
 		parent::__construct('Public');
 		
-		$this->setBO($_SESSION['currentUser']);
+		if(isset($_SESSION['currentUser']))
+			$this->setBO($_SESSION['currentUser']);
+		else
+			self::$logger->warn('Logout controller called when no user is logged in');
 		
 		// set up the title and meta details
 		$this->setTitle('Logged out successfully.');
@@ -56,6 +94,7 @@ class Logout extends AlphaController implements AlphaControllerInterface {
 	 * Handle POST requests (adds $currentUser person_object to the session)
 	 * 
 	 * @param array $params
+	 * @since 1.0
 	 */
 	public function doPOST($params) {
 		self::$logger->debug('>>doPOST($params=['.print_r($params, true).'])');
@@ -67,13 +106,16 @@ class Logout extends AlphaController implements AlphaControllerInterface {
 	 * Handle GET requests
 	 * 
 	 * @param array $params
+	 * @since 1.0
+	 * @throws AlphaException
 	 */
 	public function doGET($params) {
 		self::$logger->debug('>>doGET($params=['.print_r($params, true).'])');
 		
 		global $config;
 		
-		self::$logger->info('Logging out ['.$this->BO->get('email').'] at ['.date("Y-m-d H:i:s").']');
+		if($this->BO instanceof person_object)
+			self::$logger->info('Logging out ['.$this->BO->get('email').'] at ['.date("Y-m-d H:i:s").']');
 		
 		$_SESSION = array();
 		
@@ -81,9 +123,9 @@ class Logout extends AlphaController implements AlphaControllerInterface {
 		
 		echo AlphaView::displayPageHead($this);
 		
-		echo '<center><p class="success">You have successfully logged out of the system.</p><br>';
+		echo AlphaView::displayUpdateMessage('You have successfully logged out of the system.');
 		
-		echo '<a href="'.$config->get('sysURL').'">Home Page</a></center>';
+		echo '<div align="center"><a href="'.$config->get('sysURL').'">Home Page</a></div>';
 		
 		echo AlphaView::displayPageFoot($this);
 		
