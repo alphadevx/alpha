670e1cde9e2bb41b7dfb665799d07c19e3f7bcc4
johnc <alphadevx@users.noreply.github.com>
2011-03-22 20:11:03 +0000

    Ensuring that the validation error messages have a high enough z-index to render above JQuery UI buttons

--- scripts/validation.js
+++ scripts/validation.js
@@ -106,7 +106,7 @@
 		return html;
 	}
 
-	var errorContainer = $('<div class="ui-state-error ui-corner-all" style="display:none; position:absolute; width:auto; padding:0pt 0.7em;"><div><p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: 0.3em;"></span><span class="message"></span></p></div></div>');
+	var errorContainer = $('<div class="ui-state-error ui-corner-all" style="display:none; position:absolute; width:auto; padding:0pt 0.7em; z-index:100;"><div><p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: 0.3em;"></span><span class="message"></span></p></div></div>');
 	
 	errorContainer.css({
 		display: 'none',
