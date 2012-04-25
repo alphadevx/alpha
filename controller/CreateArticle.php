59dae4a2d6d71ba065c6e2867b732c626639da3d
alphadev <alphadevx@users.noreply.github.com>
2012-04-25 21:37:35 +0000

    #12 - adding the admin menu HTML fragment to the create/edit article controllers.

--- controller/CreateArticle.php
+++ controller/CreateArticle.php
@@ -227,6 +227,22 @@ class CreateArticle extends AlphaController implements AlphaControllerInterface 
 		
 		return $html;
 	}
+	
+	/**
+	 * Use this callback to inject in the admin menu template fragment for admin users of
+	 * the backend only.
+	 * 
+	 * @since 1.2
+	 */
+	public function after_displayPageHead_callback() {
+		$menu = '';
+		
+		if (isset($_SESSION['currentUser']) && AlphaDAO::isInstalled() && $_SESSION['currentUser']->inGroup('Admin') && strpos($_SERVER['REQUEST_URI'], '/tk/') !== false) {
+			$menu .= AlphaView::loadTemplateFragment('html', 'adminmenu.phtml', array());
+		}
+		
+		return $menu;
+	}
 }
 
 // now build the new controller
