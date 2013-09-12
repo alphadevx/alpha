c982d362e0e3b5584abdcf8d168df6be9e822e92
alphadevx <alphadevx@1970b06b-b575-2e47-bfa4-2824af2bf091>
2013-09-12 20:41:08 +0000

    #78 - fixed broken support for the security.encrypt.http.fieldnames in a view more core views and controllers

--- trunk/controller/ViewImage.php
+++ trunk/controller/ViewImage.php
@@ -4,7 +4,7 @@
 if(!isset($config)) {
 	require_once '../util/AlphaConfig.inc';
 	$config = AlphaConfig::getInstance();
-	
+
 	require_once $config->get('app.root').'alpha/util/AlphaAutoLoader.inc';
 }
 
