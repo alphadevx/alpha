b8f0d89f61889410a8c01d87ce3338c56437fc39
alphadevx <alphadevx@1970b06b-b575-2e47-bfa4-2824af2bf091>
2014-10-29 22:47:30 +0000

    Removing bad 1.2.4 tag

--- tags/1.2.4/scripts/force-frame.js
+++
@@ -1,8 +0,0 @@
-pageLoc = self.location;
-pageAdd = top.location;
-
-if (pageLoc == pageAdd) {
-	contentSrc = escape(pageLoc);
-	contPage = '../index.php?' + contentSrc;
-	top.location.href = contPage;
-}
