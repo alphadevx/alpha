bd325f81be1e9ce269e3200dd019b7278199c38b
alphadevx <john@alphadevx.com>
2014-12-04 23:54:50 +0000

    #115 - moved model and error handler unit tests to namespaces

---
+++ test/Alpha/Test/Model/ModelTestCase.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Alpha\Test\Model;
+
+use Alpha\Util\Config\ConfigProvider;
+use Alpha\Model\ActiveRecord;
+
+/**
+ *
+ * Test class used by tests that need to write to the unit test database.
+ *
+ * @since 2.0
+ * @author John Collins <dev@alphaframework.org>
+ * @version $Id: DEnumTest.php 1845 2014-11-18 22:18:55Z alphadevx $
+ * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @copyright Copyright (c) 2015, John Collins (founder of Alpha Framework).
+ * All rights reserved.
+ *
+ * <pre>
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * * Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer in the documentation and/or other
+ *   materials provided with the distribution.
+ * * Neither the name of the Alpha Framework nor the names
+ *   of its contributors may be used to endorse or promote
+ *   products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * </pre>
+ *
+ */
+class ModelTestCase extends \PHPUnit_Framework_TestCase
+{
+	/**
+     * Switches to using the test database
+     *
+     * @since 2.0
+     */
+    protected function setUp()
+    {
+        $config = ConfigProvider::getInstance();
+        // flip the standard database settings with the test ones
+		$config->set('db.name', $config->get('db.test.name'));
+		$config->set('db.username', $config->get('db.test.username'));
+		$config->set('db.password', $config->get('db.test.password'));
+		$config->set('db.hostname', $config->get('db.test.hostname'));
+		// do the same for the SQLite3 provider
+		$config->set('db.file.path', $config->get('db.file.test.path'));
+		// force a disconnect to break any existing connections to the main database
+		ActiveRecord::disconnect();
+		// TODO create the test database
+    }
+
+    /**
+     * Drop the test database between tests
+     *
+     * @since 2.0
+     */
+    protected function tearDown()
+    {
+    	// TODO drop the test database between tests
+    }
+}
+
+?>
