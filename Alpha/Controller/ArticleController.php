7dbb045d12ad4391a0ff51ecd53e39da54890a52
alphadevx <john@alphadevx.com>
2015-01-01 18:57:23 +0000

    #115 - moved the EditController::doPOST method to ArticleController::doPUT, and removed the non-namespaced EditController class

--- Alpha/Controller/ArticleController.php
+++ Alpha/Controller/ArticleController.php
@@ -5,10 +5,15 @@ namespace Alpha\Controller;
 use Alpha\Util\Logging\Logger;
 use Alpha\Util\Config\ConfigProvider;
 use Alpha\Util\Security\SecurityUtils;
+use Alpha\Util\Helper\Validator;
 use Alpha\Model\Article;
 use Alpha\View\View;
+use Alpha\View\ViewState;
+use Alpha\View\Widget\Button;
 use Alpha\Exception\SecurityException;
 use Alpha\Exception\AlphaException;
+use Alpha\Exception\RecordNotFoundException;
+use Alpha\Exception\LockingException;
 use Alpha\Model\ActiveRecord;
 use Alpha\Controller\Front\FrontController;
 
@@ -90,11 +95,6 @@ class ArticleController extends Controller implements ControllerInterface
 
         $this->BO = new Article();
 
-        // set up the title and meta details
-        $this->setTitle('Create a new Article');
-        $this->setDescription('Page to create a new article.');
-        $this->setKeywords('create,new,article');
-
         self::$logger->debug('<<__construct');
     }
 
@@ -108,11 +108,44 @@ class ArticleController extends Controller implements ControllerInterface
     {
         self::$logger->debug('>>doGET($params=['.var_export($params, true).'])');
 
-        echo View::displayPageHead($this);
+        // editing
+        if (isset($params['oid'])) {
+            if (!Validator::isInteger($params['oid']))
+                throw new IllegalArguementException('Article ID provided ['.$params['oid'].'] is not valid!');
 
-        $view = View::getInstance($this->BO);
+            try {
+                $this->BO->load($params['oid']);
+            } catch (RecordNotFoundException $e) {
+                self::$logger->warn($e->getMessage());
+                echo View::renderErrorPage(404, 'Failed to find the requested article!');
+                return;
+            }
 
-        echo $view->createView();
+            ActiveRecord::disconnect();
+
+            $view = View::getInstance($this->BO);
+
+            // set up the title and meta details
+            $this->setTitle($this->BO->get('title').' (editing)');
+            $this->setDescription('Page to edit '.$this->BO->get('title').'.');
+            $this->setKeywords('edit,article');
+
+            echo View::displayPageHead($this);
+
+            echo $view->editView();
+            echo View::renderDeleteForm();
+        } else { // creating
+            $view = View::getInstance($this->BO);
+
+            // set up the title and meta details
+            $this->setTitle('Creating article');
+            $this->setDescription('Page to create a new article.');
+            $this->setKeywords('create,article');
+
+            echo View::displayPageHead($this);
+
+            echo $view->createView();
+        }
 
         echo View::displayPageFoot($this);
 
@@ -123,7 +156,7 @@ class ArticleController extends Controller implements ControllerInterface
      * Method to handle POST requests
      *
      * @param array $params
-     * @throws SecurityException
+     * @throws Alpha\Exception\SecurityException
      * @since 1.0
      */
     public function doPOST($params)
@@ -168,6 +201,151 @@ class ArticleController extends Controller implements ControllerInterface
             echo '<p class="error"><br>'.$e->getMessage().'</p>';
             self::$logger->warn($e->getMessage());
         }
+
+        self::$logger->debug('<<doPOST');
+    }
+
+    /**
+     * Method to handle PUT requests
+     *
+     * @param array $params
+     * @since 1.0
+     */
+    public function doPUT($params)
+    {
+        self::$logger->debug('>>doPUT(params=['.var_export($params, true).'])');
+
+        $config = ConfigProvider::getInstance();
+
+        try {
+            // check the hidden security fields before accepting the form POST data
+            if (!$this->checkSecurityFields()) {
+                throw new SecurityException('This page cannot accept post data from remote servers!');
+                self::$logger->debug('<<doPUT');
+            }
+
+            if (isset($params['markdownTextBoxRows']) && $params['markdownTextBoxRows'] != '') {
+                $viewState = ViewState::getInstance();
+                $viewState->set('markdownTextBoxRows', $params['markdownTextBoxRows']);
+            }
+
+            if (isset($params['oid'])) {
+                if (!Validator::isInteger($params['oid']))
+                    throw new IllegalArguementException('Article ID provided ['.$params['oid'].'] is not valid!');
+
+                $this->BO->load($params['oid']);
+
+                $View = View::getInstance($this->BO);
+
+                // set up the title and meta details
+                $this->setTitle($this->BO->get('title').' (editing)');
+                $this->setDescription('Page to edit '.$this->BO->get('title').'.');
+                $this->setKeywords('edit,article');
+
+                echo View::displayPageHead($this);
+
+                if (isset($params['saveBut'])) {
+
+                    // populate the transient object from post data
+                    $this->BO->populateFromPost();
+
+                    try {
+                        $success = $this->BO->save();
+                        self::$logger->action('Article '.$this->BO->getID().' saved');
+                        echo View::displayUpdateMessage('Article '.$this->BO->getID().' saved successfully.');
+                    } catch (LockingException $e) {
+                        $this->BO->reload();
+                        echo View::displayErrorMessage($e->getMessage());
+                    }
+
+                    ActiveRecord::disconnect();
+                    echo $View->editView();
+                }
+
+                if (!empty($params['deleteOID'])) {
+
+                    $this->BO->load($params['deleteOID']);
+
+                    try {
+                        $this->BO->delete();
+                        self::$logger->action('Article '.$params['deleteOID'].' deleted.');
+                        ActiveRecord::disconnect();
+
+                        echo View::displayUpdateMessage('Article '.$params['deleteOID'].' deleted successfully.');
+
+                        echo '<center>';
+
+                        $temp = new Button("document.location = '".FrontController::generateSecureURL('act=ListAll&bo='.get_class($this->BO))."'",
+                            'Back to List','cancelBut');
+                        echo $temp->render();
+
+                        echo '</center>';
+                    } catch (AlphaException $e) {
+                        self::$logger->error($e->getTraceAsString());
+                        echo View::displayErrorMessage('Error deleting the article, check the log!');
+                    }
+                }
+
+                if (isset($params['uploadBut'])) {
+
+                    // upload the file to the attachments directory
+                    $success = move_uploaded_file($_FILES['userfile']['tmp_name'], $this->BO->getAttachmentsLocation().'/'.$_FILES['userfile']['name']);
+
+                    if (!$success)
+                        throw new AlphaException('Could not move the uploaded file ['.$_FILES['userfile']['name'].']');
+
+                    // set read/write permissions on the file
+                    $success = chmod($this->BO->getAttachmentsLocation().'/'.$_FILES['userfile']['name'], 0666);
+
+                    if (!$success)
+                        throw new AlphaException('Unable to set read/write permissions on the uploaded file ['.$this->BO->getAttachmentsLocation().'/'.$_FILES['userfile']['name'].'].');
+
+                    if ($success) {
+                        echo View::displayUpdateMessage('File uploaded successfully.');
+                        self::$logger->action('File '.$_FILES['userfile']['name'].' uploaded to '.$this->BO->getAttachmentsLocation().'/'.$_FILES['userfile']['name']);
+                    }
+
+                    $view = View::getInstance($this->BO);
+
+                    echo $view->editView();
+                }
+
+                if (!empty($params['file_to_delete'])) {
+
+                    $success = unlink($this->BO->getAttachmentsLocation().'/'.$params['file_to_delete']);
+
+                    if (!$success)
+                        throw new AlphaException('Could not delete the file ['.$params['file_to_delete'].']');
+
+                    if ($success) {
+                        echo View::displayUpdateMessage($params['file_to_delete'].' deleted successfully.');
+                        self::$logger->action('File '.$this->BO->getAttachmentsLocation().'/'.$params['file_to_delete'].' deleted');
+                    }
+
+                    $view = View::getInstance($this->BO);
+
+                    echo $view->editView();
+                }
+            } else {
+                throw new IllegalArguementException('No valid article ID provided!');
+            }
+        } catch (SecurityException $e) {
+            echo View::displayErrorMessage($e->getMessage());
+            self::$logger->warn($e->getMessage());
+        } catch (IllegalArguementException $e) {
+            echo View::displayErrorMessage($e->getMessage());
+            self::$logger->error($e->getMessage());
+        } catch (RecordNotFoundException $e) {
+            self::$logger->warn($e->getMessage());
+            echo View::displayErrorMessage('Failed to load the requested article from the database!');
+        } catch (AlphaException $e) {
+            echo View::displayErrorMessage($e->getMessage());
+            self::$logger->error($e->getMessage());
+        }
+
+        echo View::renderDeleteForm();
+
+        echo View::displayPageFoot($this);
 
         self::$logger->debug('<<doPOST');
     }
