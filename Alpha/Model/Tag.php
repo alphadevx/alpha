93deffe93116bc8901365200540c8471c15adb03
alphadevx <john@alphadevx.com>
2019-05-05 20:30:12 +0100

    #349 - added the optional ownerID param to the Tag::getPopularTagsArray() method

--- Alpha/Model/Tag.php
+++ Alpha/Model/Tag.php
@@ -21,7 +21,7 @@ use Exception;
  *
  * @author John Collins <dev@alphaframework.org>
  * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
- * @copyright Copyright (c) 2018, John Collins (founder of Alpha Framework).
+ * @copyright Copyright (c) 2019, John Collins (founder of Alpha Framework).
  * All rights reserved.
  *
  * <pre>
@@ -171,7 +171,8 @@ class Tag extends ActiveRecord
      * ordered by alphabet and restricted to the a count matching the $limit supplied.  The
      * returned has array uses the tag content as a key and the database value as a value.
      *
-     * @param $limit
+     * @param $limit   The maximum amount of tags to return
+     * @param $ownerID The ID of the owner of the tags that we should load by (optional)
      *
      * @return array
      *
@@ -179,14 +180,14 @@ class Tag extends ActiveRecord
      *
      * @throws \Alpha\Exception\AlphaException
      */
-    public static function getPopularTagsArray($limit)
+    public static function getPopularTagsArray($limit, $ownerID = '')
     {
         $config = ConfigProvider::getInstance();
 
         $provider = ServiceFactory::getInstance($config->get('db.provider.name'), 'Alpha\Model\ActiveRecordProviderInterface');
         $provider->setRecord(new self());
 
-        $sqlQuery = 'SELECT content, count(*) as count FROM '.self::TABLE_NAME." GROUP BY content ORDER BY count DESC LIMIT $limit";
+        $sqlQuery = 'SELECT content, count(*) as count FROM '.self::TABLE_NAME.($ownerID == '' ? '' : " WHERE created_by = '".$ownerID."'")." GROUP BY content ORDER BY count DESC LIMIT $limit";
 
         try {
             $result = $provider->query($sqlQuery);
@@ -316,7 +317,7 @@ class Tag extends ActiveRecord
                 $cache->delete($this->get('content'));
             } catch (\Exception $e) {
                 self::$logger->error('Error while attempting to remove search matches array from the ['.$config->get('cache.provider.name').'] 
-	      			instance: ['.$e->getMessage().']');
+                    instance: ['.$e->getMessage().']');
             }
         }
     }
