c9d958eb729ec714a87964d1278f7396c04d7468
alphadevx <john@alphadevx.com>
2019-12-30 22:31:03 +0000

    #229 - increased the test coverage on the RendererProviderJSON class

--- test/Alpha/Test/View/ViewTest.php
+++ test/Alpha/Test/View/ViewTest.php
@@ -298,15 +298,17 @@ class ViewTest extends TestCase
         $this->assertTrue(strpos($this->view->editView(array('formAction' => '/')), 'very bad client') !== false, 'Testing the editView() method');
     }
 
+    /**
+     * Testing the displayErrorMessage() method.
+     *
+     * @since 3.1
+     */
     public function testDisplayErrorMessage()
     {
-        $config = ConfigProvider::getInstance();
-        $config->set('app.renderer.provider.name', 'Alpha\View\Renderer\Html\RendererProviderJSON');
-
+        $this->view->setProvider('Alpha\View\Renderer\Json\RendererProviderJSON');
         $this->assertTrue(strpos(View::displayErrorMessage('something went wrong'), 'something went wrong') > 0);
 
-        $config->set('app.renderer.provider.name', 'Alpha\View\Renderer\Html\RendererProviderHTML');
-
+        $this->view->setProvider('Alpha\View\Renderer\Html\RendererProviderHTML');
         $this->assertTrue(strpos(View::displayErrorMessage('something went wrong'), 'something went wrong') > 0);
     }
 }
