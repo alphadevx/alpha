95d3b0ff4f53a1a0fe3369c8a9235b1747e5cd89
alphadevx <john@alphadevx.com>
2021-06-03 15:58:35 +0100

    #376 - adding more test coverage for IndexController::doGET()

--- test/Alpha/Test/Controller/IndexControllerTest.php
+++ test/Alpha/Test/Controller/IndexControllerTest.php
@@ -5,6 +5,7 @@ namespace Alpha\Test\Controller;
 use Alpha\Controller\Front\FrontController;
 use Alpha\Controller\IndexController;
 use Alpha\Util\Http\Request;
+use Alpha\Model\Person;
 
 /**
  * Test cases for the IndexController class.
@@ -13,7 +14,7 @@ use Alpha\Util\Http\Request;
  *
  * @author John Collins <dev@alphaframework.org>
  * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
- * @copyright Copyright (c) 2018, John Collins (founder of Alpha Framework).
+ * @copyright Copyright (c) 2021, John Collins (founder of Alpha Framework).
  * All rights reserved.
  *
  * <pre>
@@ -62,5 +63,13 @@ class IndexControllerTest extends ControllerTestCase
 
         $this->assertEquals(200, $response->getStatus(), 'Testing the doGET method');
         $this->assertEquals('text/html', $response->getHeader('Content-Type'), 'Testing the doGET method');
+
+        $person = new Person();
+        $person->dropTable();
+
+        $request = new Request(array('method' => 'GET', 'URI' => '/'));
+        $response = $front->process($request);
+
+        $this->assertEquals(301, $response->getStatus(), 'Testing the doGET method re-directs to /install when ActiveRecord::isInstalled() returns false');
     }
 }
