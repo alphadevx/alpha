7ba8cf95f598f2dc9e93698846cee18c8927818c
alphadevx <john@alphadevx.com>
2022-09-13 12:23:12 +0100

    #365 - fixing the validation JS to work with Bootstrap 5.2 when validating equal-to form field comparisons

--- public/js/validation.js
+++ public/js/validation.js
@@ -9,8 +9,31 @@
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
+          Array.from(form.elements).forEach((input) => {
+            if (typeof input.pattern  !== 'undefined') {
+              
+              var rule = input.pattern;
+
+              if (typeof input.dataset.oldrule  !== 'undefined') {
+                rule = input.dataset.oldrule;
+              }
+
+              if (rule.match(/equal\ to\ /g)) {
+                var equalToField = rule.replace(/equal\ to\ /g,'');
+                if(input.value === document.getElementById(equalToField).value) {
+                
+                  input.setCustomValidity("");
+                  input.pattern = ".*";
+                  input.setAttribute('data-oldrule', rule);
+                } else {
+                  input.setCustomValidity("invalid");
+                }
+                input.reportValidity();
+              }
+            }
+          });
         }
 
         form.classList.add('was-validated')
